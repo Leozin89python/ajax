6247d3a3cd04081d1648de7b0e8d3d70e3032d81
Leozin89Python <leozin069@gmail.com>
2021-07-08 16:23:05 -0300

    ajax-request-to-api-version~1.1.65

--- systemwithlogin/versao_sem_responsividade/backend/database/Db.sql
+++ systemwithlogin/versao_sem_responsividade/backend/database/Db.sql
@@ -5,3 +5,11 @@ CREATE DATABASE knex_ajax_express;
 SELECT * FROM  pessoa; 
 
 INSERT INTO pessoa VALUES(2,'Tom Hanks','actor','cinema',3.541);
+
+SELECT id,name,department,job,salary FROM pessoa where id < 20;
+
+CREATE VIEW v_group AS 
+SELECT id,name,department,job,salary FROM pessoa ORDER BY salary;
+
+SELECT * FROM v_group;
+
